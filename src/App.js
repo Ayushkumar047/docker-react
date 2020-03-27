88e17275f7382fcfade7caa19d6a1461e117cb7c
Ayushkumar047 <pratapsingh.aayush047@gmail.com>
2020-03-27 11:05:59 +0530

    change text of app.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React on docker
+          Learn React on Docker
         </a>
       </header>
     </div>
